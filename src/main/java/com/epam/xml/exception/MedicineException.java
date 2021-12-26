d2c94ca5521ad437f7c474a6af6c85924d9f1dd9
Artem Chernenkov <temach2002@gmail.com>
2021-12-26 16:29:47 +0300

    Add Validate=or

--- src/main/java/com/epam/xml/exception/MedicineException.java
+++ src/main/java/com/epam/xml/exception/MedicineException.java
@@ -1,6 +1,18 @@
 package com.epam.xml.exception;
 
 public class MedicineException extends Exception {
+    public MedicineException() {
+        super();
+    }
+
+    public MedicineException(String message) {
+        super(message);
+    }
+
+    public MedicineException(Exception ex) {
+        super(ex);
+    }
+
     public MedicineException(String message, Exception exception) {
         super(message, exception);
     }
--- src/main/java/com/epam/xml/logic/MedicineXmlValidator.java
+++ src/main/java/com/epam/xml/logic/MedicineXmlValidator.java
@@ -1,9 +1,13 @@
 package com.epam.xml.logic;
 
+import com.epam.xml.exception.MedicineException;
+
 public class MedicineXmlValidator {
-    public boolean isValid(String xmlPath, String xsdPath) {
-        // TODO: d
+    public boolean isValid(String xmlPath, String xsdPath) throws MedicineException {
+        if (xmlPath == null || xmlPath.isEmpty() || xsdPath == null || xsdPath.isEmpty()) {
+            throw new MedicineException("Xml path or Xsd path is empty");
+        }
         return false;
+        // TODO: sdlkfj
     }
-    // TODO: write realization
 }
